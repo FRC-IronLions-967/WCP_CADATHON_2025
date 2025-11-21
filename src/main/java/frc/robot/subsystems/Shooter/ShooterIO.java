8cbfbafe9df73159f194f4649660f1f69daf41ba
331282 <148924183+331282@users.noreply.github.com>
2025-11-20 20:01:04 -0600

    More push to work on vision

--- src/main/java/frc/robot/subsystems/Shooter/ShooterIO.java
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterIO.java
@@ -2,7 +2,22 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems.Shooter;
+package frc.robot.subsystems.shooter;
+
+import org.littletonrobotics.junction.AutoLog;
 
 /** Add your docs here. */
-public class ShooterIO {}
+public interface ShooterIO {
+    @AutoLog
+    public static class ShooterIOInputs {
+        public double shooterAngle;
+        public double shooterSpeed;
+        public double feederSpeed;
+    }
+
+    public default void updateInputs(ShooterIOInputs inputs) {}
+
+    public default void moveShooter(double angle) {}
+
+    public default void runShooter(double speed) {}
+}
